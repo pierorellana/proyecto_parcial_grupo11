a5965a8df4ba1647607b8c75e0d7ab5389922748
Pierre Orellana <89483065+pierorellana@users.noreply.github.com>
2024-06-12 14:07:30 -0500

    Revert "Dev murillo"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './componentes/login/login.component';
-import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
-import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
 
-const routes: Routes = [
-
-  { path: '', redirectTo: 'cuerpo', pathMatch: 'full' },
-  { path: 'cuerpo', component: CuerpoComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'usuario', component: UsuariosComponent },
-    { path: '**', redirectTo: 'cuerpo', pathMatch: 'full' },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { LoginComponent } from './componentes/login/login.component';
-import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
-import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
-import { MatButtonModule } from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
-import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    UsuariosComponent,
-    CabeceraComponent,
-    CuerpoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    BrowserAnimationsModule,
-    MatButtonModule, 
-    MatMenuModule,
-    MatRadioModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
